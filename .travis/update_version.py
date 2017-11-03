618e1a37c3538a1c7ccf4ec7561f27daeece29f9
Fernando Pelliccioni <fpelliccioni@gmail.com>
2017-11-03 10:44:28 -0300

    fix

--- .travis/update_version.py
+++ .travis/update_version.py
@@ -12,23 +12,32 @@ def replace_version(source_file, search, replace):
         with open(source_file) as old_file:
             lines = old_file.read().split("\n")
 
-            for l in lines:
-                if l.startswith(search):
+            for line in lines:
+                if line.startswith(search):
                     new_file.write(replace)
                 else:
-                    new_file.write(l)
+                    new_file.write(line)
+                new_file.write('\n')
 
     remove(source_file)
     move(abs_path, source_file)    
-}  
 
 
 if __name__ == "__main__":
 
     print 'Number of arguments:', len(sys.argv), 'arguments.'
     print 'Argument List:', str(sys.argv)
+    print sys.argv[1:]
 
-    args = str(sys.argv)
+    for x in sys.argv[1:]:
+        print x
+
+    # args = str(sys.argv)
+    args = sys.argv[1:]
+
+    print 'args[0]:', args[0]
+    print 'args[1]:', args[1]
+    print 'args[2]:', args[2]
 
     replace_version(args[0], args[1], args[2])
 
