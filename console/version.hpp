6019f1f754700109656666a5f8c0b0cf3af763bc
Bitprim CI <ci@bitprim.org>
2017-11-07 14:32:51 +0000

    CI automated amend version number taken from VCS [skip ci]

--- console/version.hpp
+++ console/version.hpp
@@ -26,9 +26,10 @@
 // #define BITPRIM_NODE_EXE_VERSION "v0.0.0"
 // #endif
 
-#define BITPRIM_NODE_EXE_VERSION "v0.3.0-64-g18a3799"
+#define BITPRIM_NODE_EXE_VERSION "v0.3.0-65-g2a4b40d"
 
 #endif /*LIBBITCOIN_NODE_EXE_VERSION_HPP_*/
+
 
 
 
