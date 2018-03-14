608b4334daa7b2f9bac6dde7521409e70d54811a
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-03-14 14:22:27 -0300

    Feature compile time currencies (#1)
    * Compilation-Time Cryptocurrencies selection
    
    * fix
    
    * remove comments
    
    * Travis bitprim-core build: v0.3.0-93-gb42b8c5f, 325
    
    * Travis bitprim-core build: v0.3.0-94-g3d41a93d, 326
    
    * Travis bitprim-core build: v0.3.0-95-g6d1b5d5d, 327
    
    * Travis bitprim-core build: v0.3.0-96-gb668212f, 328
    
    * Travis bitprim-core build: v0.3.0-97-g8f7f5874, 329
    
    * Travis bitprim-rpc build: v0.5.0-39-g20d723f, 69
    
    * separating version and channel
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * fixes
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * some improvements
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * Travis bitprim-core build: v0.3.0-98-g72eed9a1, 331
    
    * adding more logging and version info
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * conan recipe fix
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * build fix
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * Travis bitprim-core build: v0.3.0-99-gb61075d2, 335
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * first builds without RPC
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * log changes [skip ci]
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * settings fixes
    
    * Travis bitprim-core build: v0.3.0-100-gf618736b, 336
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * fix
    
    * logging
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * Travis bitprim-core build: v0.3.0-101-gb3503df6, 337
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * cmake currency message
    
    * Travis bitprim-core build: v0.3.0-102-g053aae61, 339
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * Travis bitprim-rpc build: v0.5.0-40-g7aa1c74, 76
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * Travis bitprim-rpc build: v0.5.0-41-g83748dd, 77
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * Travis bitprim-rpc build: v0.5.0-42-ge05207d, 78
    
    * Travis bitprim-rpc build: v0.5.0-43-g376c98d, 79
    
    * Travis bitprim-rpc build: v0.5.0-44-g3925f87, 80
    
    * Travis bitprim-core build: v0.3.0-103-gd010b913, 340
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * Travis bitprim-core build: v0.3.0-104-g6b317b5d, 341
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * Travis bitprim-core build: v0.3.0-105-g37c8827d, 342
    
    * conan version requirement
    
    * Travis bitprim-core build: v0.3.0-106-g9d73158c, 343
    
    * Travis bitprim-rpc build: v0.5.0-45-g0a7e5ee, 81
    
    * CI automated amend version number taken from VCS [skip ci]
    
    * Travis bitprim-core build: v0.3.0-121-g23a90676, 344
    
    * CI automated amend version number taken from VCS [skip ci]

--- build.py
+++ build.py
@@ -1,13 +1,108 @@
-import platform
 from conan.packager import ConanMultiPackager
 import os
 import cpuid
 import copy
+import re
+import platform
+
+
+def get_content(path):
+    with open(path, 'r') as f:
+        return f.read()
+
+def get_version():
+    return get_content('conan_version')
+
+def get_channel():
+    return get_content('conan_channel')
+
+def get_conan_vars():
+    # CONAN_REFERENCE: "bitprim-core/0.7"
+    # CONAN_USERNAME: "bitprim"
+    # CONAN_LOGIN_USERNAME: "bitprim-bintray"
+    # CONAN_CHANNEL: "experimental"
+    # CONAN_UPLOAD: "https://api.bintray.com/conan/bitprim/bitprim"
+
+    # username = os.getenv("CONAN_USERNAME", get_username_from_ci() or "bitprim")
+    # channel = os.getenv("CONAN_CHANNEL", get_channel_from_ci())
+    # version = os.getenv("CONAN_VERSION", get_version())
+    
+    login_username = os.getenv("CONAN_LOGIN_USERNAME", "bitprim-bintray")
+    username = os.getenv("CONAN_USERNAME", "bitprim")
+    channel = os.getenv("CONAN_CHANNEL", get_channel())
+    version = os.getenv("CONAN_VERSION", get_version())
+    return login_username, username, channel, version
+
+def get_value_from_recipe(search_string, recipe="conanfile.py"):
+    with open(recipe, "r") as conanfile:
+        contents = conanfile.read()
+        result = re.search(search_string, contents)
+    return result
+
+def get_name_from_recipe():
+    return get_value_from_recipe(r'''name\s*=\s*["'](\S*)["']''').groups()[0]
+
+def get_user_repository(username, repository_name):
+    # https://api.bintray.com/conan/bitprim/bitprim
+    return "https://api.bintray.com/conan/{0}/{1}".format(username.lower(), repository_name)
+
+def get_conan_upload(username):
+    repository_name = os.getenv("BIPRIM_BINTRAY_REPOSITORY", "bitprim")
+    return os.getenv("CONAN_UPLOAD", get_user_repository(username, repository_name))
+
+def get_conan_remotes(username):
+    # While redundant, this moves upload remote to position 0.
+    remotes = [get_conan_upload(username)]
+
+    # # Add bincrafters repository for other users, e.g. if the package would
+    # # require other packages from the bincrafters repo.
+    # bincrafters_user = "bincrafters"
+    # if username != bincrafters_user:
+    #     remotes.append(get_conan_upload(bincrafters_user))
+    return remotes
+
+def get_os():
+    return platform.system().replace("Darwin", "Macos")
+
+def get_archs():
+    return ["x86_64"]
+    # archs = os.getenv("CONAN_ARCHS", None)
+    # if get_os() == "Macos" and archs is None:
+    #     return ["x86_64"]
+    # return split_colon_env("CONAN_ARCHS") if archs else None
+
+
+def get_builder(args=None):
+    name = get_name_from_recipe()
+    login_username, username, channel, version = get_conan_vars()
+    reference = "{0}/{1}".format(name, version)
+    upload = get_conan_upload(username)
+    remotes = os.getenv("CONAN_REMOTES", get_conan_remotes(username))
+
+    # upload_when_stable = get_upload_when_stable()
+    # stable_branch_pattern = os.getenv("CONAN_STABLE_BRANCH_PATTERN", "stable/*")
+
+    archs = get_archs()
+    builder = ConanMultiPackager(
+        args=args,
+        username=username,
+        login_username=login_username,
+        channel=channel,
+        reference=reference,
+        upload=upload,
+        remotes=remotes,
+        archs=archs,
+        # upload_only_when_stable=upload_when_stable,
+        # stable_branch_pattern=stable_branch_pattern
+        )
+
+    return builder, name
+
 
 def handle_microarchs(opt_name, microarchs, filtered_builds, settings, options, env_vars, build_requires):
-    print(microarchs)
+    # print(microarchs)
     microarchs = list(set(microarchs))
-    print(microarchs)
+    # print(microarchs)
 
     for ma in microarchs:
         opts_copy = copy.deepcopy(options)
@@ -15,9 +110,11 @@ def handle_microarchs(opt_name, microarchs, filtered_builds, settings, options, 
         filtered_builds.append([settings, opts_copy, env_vars, build_requires])
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="bitprim", channel="testing",
-                                 remotes="https://api.bintray.com/conan/bitprim/bitprim",
-                                 archs=["x86_64"])
+    # builder = ConanMultiPackager(username="bitprim", channel="testing",
+    #                              remotes="https://api.bintray.com/conan/bitprim/bitprim",
+    #                              archs=["x86_64"])
+
+    builder, name = get_builder()
 
     # builder.add_common_builds(shared_option_name="bitprim-node:shared")
     builder.add_common_builds()
@@ -29,73 +126,37 @@ if __name__ == "__main__":
 
             env_vars["BITPRIM_BUILD_NUMBER"] = os.getenv('BITPRIM_BUILD_NUMBER', '-')
 
-            opt1 = copy.deepcopy(options)
-            opt2 = copy.deepcopy(options)
 
-            opt1["bitprim-node-exe:with_rpc"] = "True"
-            opt2["bitprim-node-exe:with_rpc"] = "False"
+            opts_bch_rpc_on = copy.deepcopy(options)
+            opts_btc_rpc_on = copy.deepcopy(options)
+            # opts_ltc_rpc_on = copy.deepcopy(options)
+            opts_bch_rpc_off = copy.deepcopy(options)
+            opts_btc_rpc_off = copy.deepcopy(options)
+            # opts_ltc_rpc_off = copy.deepcopy(options)
 
+            opts_bch_rpc_off["*:currency"] = "BCH"
+            opts_bch_rpc_off["*:with_rpc"] = "False"
+            opts_btc_rpc_off["*:currency"] = "BTC"
+            opts_btc_rpc_off["*:with_rpc"] = "False"
+            # opts_ltc_rpc_off["*:currency"] = "LTC"
+            # opts_ltc_rpc_off["*:with_rpc"] = "False"
+
+            opts_bch_rpc_on["*:currency"] = "BCH"
+            opts_bch_rpc_on["*:with_rpc"] = "True"
+            opts_btc_rpc_on["*:currency"] = "BTC"
+            opts_btc_rpc_on["*:with_rpc"] = "True"
+            # opts_ltc_rpc_on["*:currency"] = "LTC"
+            # opts_ltc_rpc_on["*:with_rpc"] = "True"
 
             marchs = ["x86_64", ''.join(cpuid.cpu_microarchitecture()), "haswell", "skylake"]
-            handle_microarchs("bitprim-node-exe:microarchitecture", marchs, filtered_builds, settings, opt1, env_vars, build_requires)
-            handle_microarchs("bitprim-node-exe:microarchitecture", marchs, filtered_builds, settings, opt2, env_vars, build_requires)
 
+            handle_microarchs("*:microarchitecture", marchs, filtered_builds, settings, opts_bch_rpc_off, env_vars, build_requires)
+            handle_microarchs("*:microarchitecture", marchs, filtered_builds, settings, opts_btc_rpc_off, env_vars, build_requires)
+            # handle_microarchs("*:microarchitecture", marchs, filtered_builds, settings, opts_ltc_rpc_off, env_vars, build_requires)
 
-            # # opt1 = copy.deepcopy(options)
-            # # opt2 = copy.deepcopy(options)
-
-            # # opt1["bitprim-node-exe:with_rpc"] = "True"
-            # # opt2["bitprim-node-exe:with_rpc"] = "False"
-
-            # # filtered_builds.append([settings, opt1, env_vars, build_requires])
-            # # filtered_builds.append([settings, opt2, env_vars, build_requires])
-
-
-            # opt1 = copy.deepcopy(options)
-            # opt2 = copy.deepcopy(options)
-            # opt3 = copy.deepcopy(options)
-            # opt4 = copy.deepcopy(options)
-            # opt5 = copy.deepcopy(options)
-            # opt6 = copy.deepcopy(options)
-            # opt7 = copy.deepcopy(options)
-            # opt8 = copy.deepcopy(options)
-
-            # #TODO(fernando): Enable with_rpc
-            # # opt1["bitprim-node-exe:microarchitecture"] = "x86_64"
-            # # opt1["bitprim-node-exe:with_rpc"] = "True"
-
-            # opt2["bitprim-node-exe:microarchitecture"] = "x86_64"
-            # opt2["bitprim-node-exe:with_rpc"] = "False"
-            
-            # # opt3["bitprim-node-exe:microarchitecture"] = ''.join(cpuid.cpu_microarchitecture())
-            # # opt3["bitprim-node-exe:with_rpc"] = "True"
-
-            # opt4["bitprim-node-exe:microarchitecture"] = ''.join(cpuid.cpu_microarchitecture())
-            # opt4["bitprim-node-exe:with_rpc"] = "False"
-
-            # # opt5["bitprim-node-exe:microarchitecture"] = "haswell"
-            # # opt5["bitprim-node-exe:with_rpc"] = "True"
-
-            # opt6["bitprim-node-exe:microarchitecture"] = "haswell"
-            # opt6["bitprim-node-exe:with_rpc"] = "False"
-
-            # # opt7["bitprim-node-exe:microarchitecture"] = "skylake"
-            # # opt7["bitprim-node-exe:with_rpc"] = "True"
-
-            # opt8["bitprim-node-exe:microarchitecture"] = "skylake"
-            # opt8["bitprim-node-exe:with_rpc"] = "False"
-
-            # # filtered_builds.append([settings, opt1, env_vars, build_requires])
-            # filtered_builds.append([settings, opt2, env_vars, build_requires])
-            # # filtered_builds.append([settings, opt3, env_vars, build_requires])
-            # filtered_builds.append([settings, opt4, env_vars, build_requires])
-            # # filtered_builds.append([settings, opt5, env_vars, build_requires])
-            # filtered_builds.append([settings, opt6, env_vars, build_requires])
-            # # filtered_builds.append([settings, opt7, env_vars, build_requires])
-            # filtered_builds.append([settings, opt8, env_vars, build_requires])
-
-
-
+            handle_microarchs("*:microarchitecture", marchs, filtered_builds, settings, opts_bch_rpc_on, env_vars, build_requires)
+            handle_microarchs("*:microarchitecture", marchs, filtered_builds, settings, opts_btc_rpc_on, env_vars, build_requires)
+            # handle_microarchs("*:microarchitecture", marchs, filtered_builds, settings, opts_ltc_rpc_on, env_vars, build_requires)
 
     builder.builds = filtered_builds
     builder.run()
