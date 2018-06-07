1a8fdbc402d93baef67e29884418116a55be2e67
Bitprim CI <ci@bitprim.org>
2018-06-07 04:39:54 +0000

    CI automated amend version number taken from VCS [skip ci]

--- console/version.hpp
+++ console/version.hpp
@@ -26,9 +26,10 @@
 // #define BITPRIM_NODE_EXE_VERSION "v0.0.0"
 // #endif
 
-#define BITPRIM_NODE_EXE_VERSION "v0.3.0-406-gc57f798"
+#define BITPRIM_NODE_EXE_VERSION "v0.3.0-407-g93841b7"
 
 #endif /*BITPRIM_NODE_EXE_VERSION_HPP_*/
+
 
 
 
