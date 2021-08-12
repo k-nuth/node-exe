ccb5570c664daf2b002fcee8356ceac715a16620
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-08-12 16:27:42 -0300

    script

--- build_vscode_debug.sh
+++ build_vscode_debug.sh
@@ -2,6 +2,5 @@ mkdir build
 cd build
 # rm -rf *
 # rm -rf bin
-conan install .. -o use_domain=False -o db_transaction_unconfirmed=False -o db_spends=False -o db_history=False -o db_stealth=False -o db_unspent_legacy=False -o db_legacy=False -o db_new=True -s build_type=Debug
-# conan install .. -o use_domain=False -o db_transaction_unconfirmed=False -o db_spends=False -o db_history=False -o db_stealth=False -o db_unspent_legacy=False -o db_legacy=False -o db_new=True -s build_type=Release
+conan install .. -s build_type=Debug
 conan build ..
